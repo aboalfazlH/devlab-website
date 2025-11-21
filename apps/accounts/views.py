3b6e21547e398ba950876588f5ed0c3eb6066ea4
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-21 14:54:05 +0330

    Add Sign up

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,11 @@
+from .forms import CustomUserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from .models import CustomUser
 
-# Create your views here.
+class SignUpView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('admin:index') #  TODO : ADD LOGIN AND A TRUE SUCCESS URL
+    template_name = "sign-up.html"
