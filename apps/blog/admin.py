a77a4c01529ec8a7f9fef890750eebcc632b3cb6
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-21 19:21:53 +0330

    Add admin view for article

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,4 +2,74 @@ from django.contrib import admin
 from .models import Article
 
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    """Admin View for Article"""
+
+    list_display = (
+        "title",
+        "is_active",
+        "is_verify",
+        "is_pin",
+        "status",
+        "write_date",
+        "update_date",
+        "delete_date",
+        "verify_date",
+    )
+    list_editable = ("is_active", "is_pin", "is_verify")
+    list_filter = (
+        "is_active",
+        "is_verify",
+        "is_pin",
+        "write_date",
+        "update_date",
+        "delete_date",
+        "verify_date",
+    )
+    readonly_fields = (
+        "write_date",
+        "update_date",
+        "delete_date",
+        "verify_date",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "short_description",
+    )
+    date_hierarchy = "write_date"
+    ordering = ("-write_date", "is_active")
+
+    fieldsets = (
+        (
+            "اطلاعات اصلی",
+            {
+                "classes": ("wide",),
+                "fields": ("title", "short_description", "description", "thumbnail"),
+            },
+        ),
+        (
+            "اطلاعات ویژه",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "is_active",
+                    "is_verify",
+                    "is_pin",
+                ),
+            },
+        ),
+        (
+            "تاریخ ها",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "write_date",
+                    "update_date",
+                    "verify_date",
+                    "delete_date",
+                ),
+            },
+        ),
+    )
