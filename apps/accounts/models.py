d4cfc02cd0c06b3f839be41f0c5c27fdd91f1e35
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-23 21:19:36 +0330

    Update header

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,3 +4,6 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True,verbose_name=_("email"))
+
+    def __str__(self):
+        return f"{self.username}" if self.get_full_name() is None else self.get_full_name() 
