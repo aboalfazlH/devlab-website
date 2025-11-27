a68d8a58cf64ddbec6a13856edb32434fe1cc134
aboalfazlH <saboalfazl6162@gmail.com>
2025-11-27 08:40:25 +0330

    add Adminview for Subscription

--- apps/subscription/models.py
+++ apps/subscription/models.py
@@ -1,33 +1,53 @@
 from django.db import models
 from apps.accounts.models import CustomUser
+from django.utils import timezone
+
 
 class SubscriptionPlan(models.Model):
     """Model definition for SubscriptionPlan."""
 
-    plan_name = models.CharField()
+    plan_name = models.CharField(verbose_name="")
+    price = models.PositiveIntegerField(verbose_name="")
 
     class Meta:
         """Meta definition for SubscriptionPlan."""
 
-        verbose_name = 'پلن اشتراک'
-        verbose_name_plural = 'پلن های اشتراک'
+        verbose_name = "پلن اشتراک"
+        verbose_name_plural = "پلن های اشتراک"
 
     def __str__(self):
         """Unicode representation of SubscriptionPlan."""
-        return self.plan_name    
+        return self.plan_name
+
 
 class Subscription(models.Model):
     """Model definition for Subscription."""
-    subscription_plan = models.ForeignKey(SubscriptionPlan,on_delete=models.CASCADE)
-    subscription_user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    start_date = models.DateTimeField(auto_now_add=True)
-    end_date = models.DateTimeField()
+
+    subscription_plan = models.ForeignKey(
+        SubscriptionPlan, on_delete=models.CASCADE, verbose_name="پلن اشتراک"
+    )
+    subscription_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, verbose_name="کاربر"
+    )
+    start_date = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ شروع")
+    end_date = models.DateTimeField(verbose_name="تاریخ پایان")
+
+    @property
+    def is_active(self):
+        return self.end_date >= timezone.now()
+
     class Meta:
         """Meta definition for Subscription."""
 
-        verbose_name = 'Subscription'
-        verbose_name_plural = 'Subscriptions'
+        verbose_name = "اشتراک"
+        verbose_name_plural = "اشتراک ها"
+    def save(self, *args, **kwargs):
+        if Subscription.objects.filter(
+            subscription_user=self.subscription_user,
+            end_date__gte=timezone.now()
+        ).exclude(pk=self.pk).exists():
+            raise ValueError("این کاربر یک اشتراک فعال دارد و نمی‌تواند اشتراک جدید بگیرد.")
 
     def __str__(self):
         """Unicode representation of Subscription."""
-        f"{self.subscription_user}-{self.subscription_plan}|{self.start_date} تا {self.end_date}"
+        return f"{self.subscription_user}-{self.subscription_plan}|{self.start_date} تا {self.end_date}"
