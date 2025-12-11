8eb55e81b3a95fdae435f862ccdf61aca510660e
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2025-12-11 15:56:22 +0330

    Add quill to question

--- apps/qa/forms.py
+++ apps/qa/forms.py
@@ -1,17 +1,25 @@
 from django import forms
 from .models import Question
-from django_summernote.widgets import SummernoteWidget
-from django_select2.forms import Select2MultipleWidget
 
 class QuestionForm(forms.ModelForm):
-    """Form definition for Question."""
+    """Form definition for Question using Quill."""
 
     class Meta:
-        """Meta definition for QuestionForm."""
-
         model = Question
-        fields = ("name","help_image","question_description","slug","categories")
+        fields = ("name", "help_image", "question_description", "slug", "categories")
         widgets = {
-            "question_description":SummernoteWidget(),
-            "categories":Select2MultipleWidget()
-        }        
+            "question_description": forms.Textarea(
+                attrs={
+                    "class": "quill-editor",
+                    "style": "display:none;",
+                }
+            ),
+            "categories": forms.SelectMultiple(attrs={"class": "form-select"}),  # اگر میخواید جایگزین Select2 شود
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ not in ["Textarea", "ClearableFileInput", "SelectMultiple"]:
+                existing_classes = field.widget.attrs.get("class", "")
+                field.widget.attrs["class"] = (existing_classes + " form-control").strip()
